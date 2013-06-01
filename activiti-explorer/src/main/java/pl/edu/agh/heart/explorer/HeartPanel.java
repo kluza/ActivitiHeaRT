978ae74ba9e790b3451adee4fc48ce715e866537
Roman <roman-himself@o2.pl>
2013-06-01 21:32:27 +0200

    z grubsza działające listowanie modeli z hearta

--- activiti-explorer/src/main/java/pl/edu/agh/heart/explorer/HeartPanel.java
+++ activiti-explorer/src/main/java/pl/edu/agh/heart/explorer/HeartPanel.java
@@ -8,12 +8,25 @@
 package pl.edu.agh.heart.explorer;
 
 import org.activiti.explorer.ui.custom.DetailPanel;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.TextArea;
+import com.vaadin.ui.Tree;
 
 /** @author ja */
 public class HeartPanel extends DetailPanel {
+    private Tree modelTree;
+    private TextArea text = new TextArea();
+    
     public void attach() {
         super.attach();
-        addDetailComponent(new Label("Hello world"));
+        HorizontalLayout layout = new HorizontalLayout();
+        modelTree = ModelTree.get(this);
+        layout.addComponent(modelTree);
+        layout.addComponent(text);
+        addDetailComponent(layout);
+    }
+    
+    TextArea getText() {
+        return text;
     }
 }
--- activiti-explorer/src/main/java/pl/edu/agh/heart/explorer/ModelTree.java
+++ activiti-explorer/src/main/java/pl/edu/agh/heart/explorer/ModelTree.java
@@ -7,13 +7,80 @@
  */
 package pl.edu.agh.heart.explorer;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import pl.edu.agh.heart.comm.HeartRepository;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.ui.Tree;
 
 /** @author ja */
 public class ModelTree {
-    public Tree get() {
+    public static Tree get(HeartPanel panel) {
         Tree tree = new Tree();
-        
+        tree.setImmediate(true);
+        try {
+            HeartRepository hr = new HeartRepository();
+            Map<String, List<String>> modelMap = hr.getModelNames();
+            for (String user: modelMap.keySet()) {
+                tree.addItem(user);
+                List<String> l = modelMap.get(user);
+                if ((l == null) || l.isEmpty()) {
+                    tree.setChildrenAllowed(user, false);
+                } else {
+                    for (String model: l) {
+                        Model m = new Model(user, model);
+                        tree.addItem(m);
+                        tree.setChildrenAllowed(m, false);
+                        tree.setParent(m, user);
+                    }
+                }
+                tree.expandItemsRecursively(user);
+            }
+            tree.addListener(new ModelTreeListener(tree, panel));
+        } catch (IOException exception) {
+            String errMsg = "Couldn't connect";
+            tree.addItem(errMsg);
+            tree.setChildrenAllowed(errMsg, false);
+        } catch (Exception exception) {
+            String errMsg = "Something's fucked up";
+            tree.addItem(errMsg);
+            tree.setChildrenAllowed(errMsg, false);
+        }
         return tree;
     }
+    
+    static class Model {
+        public String user, name;
+        
+        public Model(String user, String name) {
+            this.user = user;
+            this.name = name;
+        }
+        
+        public String toString() {
+            return name;
+        }
+    }
+}
+
+
+class ModelTreeListener implements ValueChangeListener {
+    private Tree tree;
+    private HeartPanel panel;
+    
+    public ModelTreeListener(Tree tree, HeartPanel panel) {
+        this.tree = tree;
+        this.panel = panel;
+    }
+    
+    public void valueChange(ValueChangeEvent pEvent) {
+        Object value = tree.getValue();
+        if (value instanceof ModelTree.Model) {
+            panel.getText().setValue(
+                    "" + ((ModelTree.Model) value).user + "/" + ((ModelTree.Model) value).name);
+        }
+    }
+    
 }
